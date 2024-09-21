8ec8fc1c922c4ecd882080185a0515b38f574525
LC-jhony <jhonyapm94@gmail.com>
2024-09-21 16:49:44 -0500

    dsdfsfsdf

---
+++ database/migrations/0001_01_00_213035_create_tenants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('domain')->nullable();
+            $table->enum('suscription_type', ['mensual', 'anual', 'demo'])->default('mensual');
+            $table->date('date_bill')->nullable();
+            $table->integer('amount');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenants');
+    }
+};
